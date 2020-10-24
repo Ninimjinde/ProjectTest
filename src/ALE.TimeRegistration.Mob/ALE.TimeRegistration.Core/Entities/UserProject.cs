e8a1555f28633603646448e69fe0f72f5f3104fa
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-10-24 09:21:45 +0200

    Create UserProject Entity

---
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Core/Entities/UserProject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALE.TimeRegistration.Core.Entities
+{
+    public class UserProject
+    {
+        public Guid UserId { get; set; }
+        public Guid ProjectId { get; set; }
+        public User User { get; set; }
+        public Project Project { get; set; }
+    }
+}
