5480d0984f53bbae5d6218ee93a21215b6a88ea1
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-10-31 20:13:05 +0100

    Create Pages

---
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/Pages/ProjectPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ALE.TimeRegistration.Mob
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProjectPage : ContentPage
+    {
+        public ProjectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
