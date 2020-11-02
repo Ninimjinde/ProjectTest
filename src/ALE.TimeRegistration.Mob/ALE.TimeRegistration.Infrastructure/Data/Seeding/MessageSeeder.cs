7289c7be5d7155e9c88a2f15d057ad477007f56e
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-11-02 17:09:36 +0100

    Seeding MessageSeeder

--- src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Infrastructure/Data/Seeding/MessageSeeder.cs
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Infrastructure/Data/Seeding/MessageSeeder.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using ALE.TimeRegistration.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ALE.TimeRegistration.Infrastructure.Data.Seeding
 {
-    class MessageSeeder
+    public class MessageSeeder
     {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Message>().HasData(
+                new Message {Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" },
+                new Message { Id = Guid.Parse(""), TaskId = Guid.Parse(""), ReceiverId = Guid.Parse(""), SenderId = Guid.Parse(""), Text = "" }
+                );
+        }
     }
 }
