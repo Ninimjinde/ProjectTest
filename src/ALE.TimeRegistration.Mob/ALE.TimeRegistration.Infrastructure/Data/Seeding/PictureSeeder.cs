208c3fb05f0ab1921017326799cc2657a79181da
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-11-02 17:09:55 +0100

    Seed PictureSeeder

--- src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Infrastructure/Data/Seeding/PictureSeeder.cs
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Infrastructure/Data/Seeding/PictureSeeder.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using ALE.TimeRegistration.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ALE.TimeRegistration.Infrastructure.Data.Seeding
 {
-    class PictureSeeder
+    public class PictureSeeder
     {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Picture>().HasData(
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("")},
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("") },
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("") },
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("") },
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("") },
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("") },
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("") },
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("") },
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("") },
+                new Picture { Id = Guid.Parse(""), TaskId = Guid.Parse(""), Image = new Uri("") }
+                );
+        }
     }
 }
