4d94890ce211a7dfa6e2cf1f905ba3213c2e512e
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-11-08 10:30:13 +0100

    Add TaskResponseDto

---
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Core/Dtos/TaskResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using ALE.TimeRegistration.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALE.TimeRegistration.Core.Dtos
+{
+    public class TaskResponseDto : DtoBase
+    {
+        public string TaskName { get; set; }
+        public string Info { get; set; }
+        public Project Project { get; set; }
+        public ICollection<PictureResponseDto> Pictures { get; set; }
+    }
+}
