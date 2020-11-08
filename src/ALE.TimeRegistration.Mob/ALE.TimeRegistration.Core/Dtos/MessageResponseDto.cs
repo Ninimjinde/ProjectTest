687b5f5f2c9a14f85f29d11788514abc2b15c9ae
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-11-08 10:32:22 +0100

    Add MessageResponseDto

---
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Core/Dtos/MessageResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using ALE.TimeRegistration.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALE.TimeRegistration.Core.Dtos
+{
+    public class MessageResponseDto : DtoBase
+    {
+        public string Text { get; set; }
+        public Task Task { get; set; }
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+    }
+}
