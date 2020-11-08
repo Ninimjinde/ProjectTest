917004b7177d973168aa641ee2920fc1d7dd5e95
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-11-08 10:30:29 +0100

    Add PictureResponseDto

---
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Core/Dtos/PictureResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using ALE.TimeRegistration.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALE.TimeRegistration.Core.Dtos
+{
+    public class PictureResponseDto : DtoBase
+    {
+        public Uri Image { get; set; }
+        public Task Task { get; set; }
+    }
+}
