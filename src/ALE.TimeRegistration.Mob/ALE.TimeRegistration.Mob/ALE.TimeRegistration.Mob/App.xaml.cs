d4ef552dd54d29ea6204f0dfcb86ca86c2d31fbd
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-11-11 22:42:05 +0100

    ProjectPage navigation added

--- src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/App.xaml.cs
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ALE.TimeRegistration.Mob
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
--- src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/Pages/ProjectPage.xaml.cs
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/Pages/ProjectPage.xaml.cs
@@ -16,5 +16,51 @@ namespace ALE.TimeRegistration.Mob
         {
             InitializeComponent();
         }
+
+        protected async override void OnAppearing()
+        {
+            await RefreshProjectLists();
+            base.OnAppearing();
+        }
+
+        private async void BtnSettings_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConfigPage());
+        }
+
+        private async void btnStart_Clicked(object sender, EventArgs e)
+        {
+            await StartTimer();
+        }
+        private async void btnInfo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new InfoPage());
+        }
+        private async void btnStop_Clicked(object sender, EventArgs e)
+        {
+            await StopTimer();
+        }
+        private async void btnPhoto_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException(); //open native function camera
+        }
+
+        private async Task LvProjectLists_ItemTapped()
+        {
+            throw new NotImplementedException();
+        }
+        
+        private async Task RefreshProjectLists()
+        {
+            throw new NotImplementedException();
+        }
+        private async Task StartTimer()
+        {
+            throw new NotImplementedException();
+        }
+        private async Task StopTimer()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
