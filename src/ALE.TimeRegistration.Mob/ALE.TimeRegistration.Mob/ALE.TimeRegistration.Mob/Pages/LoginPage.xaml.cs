3061b29094c2675b8fe23829299e315f13b14e54
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-11-11 22:45:16 +0100

    Add navigation to loginpage

--- src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/Pages/LoginPage.xaml.cs
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/Pages/LoginPage.xaml.cs
@@ -16,5 +16,17 @@ namespace ALE.TimeRegistration.Mob
         {
             InitializeComponent();
         }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+        private async void btnFingerprintLogin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConfigPage()); // native app fingerprint then navigate to Projectpage if ok
+        }
+        private async void btnUserLogin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConfigPage()); //check db for user and password then navigate to Projectpage if ok
+        }
     }
 }
