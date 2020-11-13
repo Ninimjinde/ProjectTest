d00be47b880ee6d2a56ac6c885074f01027923db
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-11-13 08:39:25 +0100

    Create ProjectsController

---
+++ src/ALE.TimeRegistration.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ALE.TimeRegistration.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ALE.TimeRegistration.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProjects()
+        {
+            var projects = await _projectService.ListAllAsync();
+            return Ok(projects);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(Guid id)
+        {
+            var project = await _projectService.GetByIdAsync(id);
+            if (project == null)
+            {
+                return NotFound($"Project with Id {id} does not exist.");
+            }
+
+            return Ok(project);
+        }
+    }
+}
