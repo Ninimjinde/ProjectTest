ae2ccf66c9cd93a127018478918005a54be47537
Anthony Lefevre <Anthony.Lefevre@valcredo.com>
2020-11-20 23:02:29 +0100

    Set up Static Resources

--- src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/App.xaml.cs
+++ src/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/ALE.TimeRegistration.Mob/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ALE.TimeRegistration.Mob
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new LoginPage());
+            MainPage = new NavigationPage(new Login());
         }
 
         protected override void OnStart()
